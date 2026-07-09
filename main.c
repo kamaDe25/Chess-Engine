4368ed4dbd5428ec42cb5fc0d9ce42dc83d82786
kamaDe25 <kamalikade25@gmail.com>
2026-07-09 11:10:02 -0700

    first push

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "defs.h"
+
+int main(){
+
+    //Called vice.c in the given tutorials
+
+    //Things to keep track of -------------------------------------------------------
+    //Fifty Moves
+        //counter to keep track of moves
+        //if Fifty Moves have been taken make it a draw
+
+    // 3xRep
+        //need to keep track of positions 
+        //draw if same exact board position happens three times in a game
+    //En Passant
+        //keep track of the en passant square if available
+
+    //Side
+        //keep track of what side is playing
+
+    //ply
+        //keep track of how many half moves have happened.
+    return 0;
+}
